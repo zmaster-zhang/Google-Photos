803bcf7a6d6485cd3aec399e608078cdc608328e
zmaster_zhang <zmaster@gmail.com>
2017-10-27 12:42:15 +0800

    Initial Commit

---
+++ Google Photos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Google Photos
+//
+//  Created by zhang honghai on 2017/10/27.
+//  Copyright © 2017年 zhang honghai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
